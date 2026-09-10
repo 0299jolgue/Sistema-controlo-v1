67679edad361ca34e9006af37e74618f82dea616
0299jolgue <jolgue003@gmail.com>
2026-09-10 21:01:21 +0100

    refactor: replace starter.py with main.py

--- starter.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    import uvicorn
-    from app.config import settings
-    uvicorn.run(app, host=settings.HOST, port=settings.PORT)
